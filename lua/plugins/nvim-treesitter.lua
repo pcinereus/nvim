5a7cc3a4433bc5849ad5b54b7fb2eb0b13cd7eb7
Murray Logan <i.obesulus@gmail.com>
2023-10-30 12:36:29 +1000

    Modify nvim treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -39,10 +39,47 @@ local config = function()
 			keymaps = {
 				init_selection = "<C-s>",
 				node_incremental = "<C-s>",
-				scope_incremental = false,
+				scope_incremental = grc,
 				node_decremental = "<BS>",
+            			-- init_selection = "gnn",
+            			-- node_incremental = "grn",
+            			-- scope_incremental = "grc",
+            			-- node_decremental = "grm",
 			},
 		},
+		textobjects = {
+          		select = {
+			       enable = true,
+            		       lookahead = true,
+            		       keymaps = {
+              			       -- You can use the capture groups defined in textobjects.scm
+              			       ['af'] = '@function.outer',
+              			       ['if'] = '@function.inner',
+              			       ['ac'] = '@class.outer',
+              			       ['ic'] = '@class.inner',
+            			},
+          		},
+			move = {
+            		     enable = true,
+            		     set_jumps = true, -- whether to set jumps in the jumplist
+            		     goto_next_start = {
+			     		[']m'] = '@function.outer',
+              				[']]'] = '@class.inner',
+            		},
+            		goto_next_end = {
+              			[']M'] = '@function.outer',
+              			[']['] = '@class.outer',
+            		},
+            		goto_previous_start = {
+              			['[m'] = '@function.outer',
+				['[['] = '@class.inner',
+			},
+            		goto_previous_end = {
+              			['[M'] = '@function.outer',
+              			['[]'] = '@class.outer',
+			},
+          	},
+            },
 	})
 end
 
